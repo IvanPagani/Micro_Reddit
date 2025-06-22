8d2ac29570247b33dd4da24ffbfaa5534d4bf682
IvanPagani <ivanpagani07@gmail.com>
2025-06-22 12:48:01 -0300

    Added Users validation and saved a new User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
-class User < ApplicationRecord
+class User < ApplicationRecord  
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
