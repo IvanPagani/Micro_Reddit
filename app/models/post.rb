4d3549bd8d03db35214eae76b2a84144b8a63404
IvanPagani <ivanpagani07@gmail.com>
2025-06-24 15:37:19 -0300

    Added Comments model and migration

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
   
-  validates :title, presence: true, length: { maximum: 50 }
-  validates :body, presence: true, length: { minimum: 10 }
+  validates :title, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :body, presence: true, length: { minimum: 10, maxumum: 1000 }
 end
